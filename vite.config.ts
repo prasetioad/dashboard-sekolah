bfaeaa8d2c00db8a30c99ade8ad5e77d8c7a5984
Wisnu <prasetioad@gmail.com>
2025-04-16 13:51:02 +0700

    fix config

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,13 @@
-// vite.config.ts
+import path from "path";
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path"; // error ini karena ESM
-
-// Ganti "__dirname" dengan cara ESM
-import { fileURLToPath } from "url";
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
